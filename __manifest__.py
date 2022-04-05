2afbaf30b622429e6bc1e8d10a19942125da2b50
rehan <frayhands@gmail.com>
2022-04-05 21:22:55 +0700

    add(module dc partner)

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "dc_partner",
+    'summary': """
+         Dewi coryati partner customize
+         """,
+
+    'description': """
+         Dewi coryati partner customize
+    """,
+    'author': "Rehan | Fahmi Roihanul Firdaus",
+    'website': "https://www.frayhands.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': [
+        'base',
+        'contacts'
+    ],
+
+    'data': [
+        'views/partner_views.xml',
+    ]
+}
